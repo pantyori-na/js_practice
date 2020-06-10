1fcb595b48cc5cc6360f5c694857ec43a1c5aaf9
pantyori-na <kouzu.keinaka8@gmail.com>
2020-06-10 12:05:31 +0900

    complete-checkbox-function

--- todolist/app.js
+++ todolist/app.js
@@ -1,16 +1,17 @@
-const addTask = document.querySelector('.add');
-const list = document.querySelector('.todos');
-const search = document.querySelector('.search input');
-const inputs = document.querySelectorAll('input[type="checkbox"]');
+const addTask = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 const checkElement = document.getElementById("check");
+var statusArray = [];
 
-(function () {
-  console.log('即時関数');
+(function (e) {
+  console.log("即時関数");
   // 初期化処理
+
   // ローカルストレージに格納されている値を取得し、リストを生成する
   for (var key in localStorage) {
     // 勝手にロードするとclientIdが保存されてしまっていたので出力させない
-    if (!(key.includes("ClientId") || key.includes("inputs"))) {
+    if (!key.includes("ClientId")) {
       var Data = JSON.parse(localStorage.getItem(key));
       if (Data) {
         var html = Data.html;
@@ -18,16 +19,21 @@ const checkElement = document.getElementById("check");
         if (html) {
           list.innerHTML += html;
         }
-        if (status) {
-
-        }
+        statusArray.push(status);
       }
     }
   }
+  var inputs = list.querySelectorAll('input[type="checkbox"]');
+  inputs.forEach((input, index) => {
+    if (statusArray[index] === true) {
+      input.checked = statusArray[index];
+      var spanClass = input.nextSibling.classList;
+      spanClass.toggle("done");
+    }
+  });
 })();
 
-
-const createTodoList = task => {
+const createTodoList = (task) => {
   // HTML テンプレートを生成
   const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -38,12 +44,12 @@ const createTodoList = task => {
 
   var Data = {
     html: html,
-    status: false
+    status: false,
   };
 
   list.innerHTML += Data.html;
   saveTaskTolocalStorage(task, Data);
-}
+};
 
 // taskの保存
 const saveTaskTolocalStorage = (task, Data) => {
@@ -54,26 +60,23 @@ const saveTaskTolocalStorage = (task, Data) => {
     return;
   }
   return;
-}
-
+};
 
 // ====オリジナル追加======
 // checkboxがクリックされたら横線をいれる
-list.addEventListener('click', e => {
-
-  if (e.target.nodeName === 'INPUT') {
+list.addEventListener("click", (e) => {
+  if (e.target.nodeName === "INPUT") {
     var key = e.target.parentElement.textContent.trim();
     var Data = JSON.parse(localStorage.getItem(key));
     // checkboxの次の要素がspanタグ
     var spanClass = e.target.nextSibling.classList;
-    spanClass.toggle('done');
+    spanClass.toggle("done");
     checkboxToLocalStorage(key, spanClass, Data);
   }
 });
 
 // checkboxの保存
 const checkboxToLocalStorage = (key, spanClass, Data) => {
-
   if (spanClass.value === "done") {
     Data.status = true;
   } else {
@@ -81,30 +84,16 @@ const checkboxToLocalStorage = (key, spanClass, Data) => {
   }
 
   localStorage.setItem(key, JSON.stringify(Data));
-}
-
-
-// checkboxの呼び出し
-const checkboxLoad = () => {
-
-
-
-  // let inputs = JSON.parse(localStorage.getItem('inputs'));
-  // if (inputs) {
-  //   inputs.forEach((input) => {
-  //     document.getElementById(input.id).checked = input.checked;
-  //   });
-  // }
-}
+};
 
 // 削除の保存機能
-const deleteTaskFromlocalStorage = task => {
+const deleteTaskFromlocalStorage = (task) => {
   localStorage.removeItem(task);
   return;
-}
+};
 
 // task追加機能
-addTask.addEventListener('submit', e => {
+addTask.addEventListener("submit", (e) => {
   // デフォルトのイベンドを無効
   e.preventDefault();
 
@@ -115,11 +104,16 @@ addTask.addEventListener('submit', e => {
     createTodoList(task);
     addTask.reset();
   }
+
+  var inputs = list.querySelectorAll('input[type="checkbox"]');
+  inputs.forEach((input, index) => {
+    input.checked = statusArray[index];
+  });
 });
 
 //　削除機能
-list.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
 
     const task = e.target.parentElement.textContent.trim();
@@ -132,14 +126,14 @@ const filterTasks = (term) => {
   Array.from(list.children)
     // フィルタ条件
     .filter((todo) => !todo.textContent.toLowerCase().includes(term))
-    .forEach((todo) => todo.classList.add('filtered'));
+    .forEach((todo) => todo.classList.add("filtered"));
 
   Array.from(list.children)
     .filter((todo) => todo.textContent.toLowerCase().includes(term))
-    .forEach((todo) => todo.classList.remove('filtered'));
+    .forEach((todo) => todo.classList.remove("filtered"));
 };
 
-search.addEventListener('keyup', () => {
+search.addEventListener("keyup", () => {
   // 空白削除かつ、小文字に変換(大文字・小文字の区別をなくす)
   const term = search.value.trim().toLowerCase();
   filterTasks(term);
